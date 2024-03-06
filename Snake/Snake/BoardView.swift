b81e5427d877fb33a0ef00c42cdcfcdf39cea7fc
zeynep <zeynep.eminoglu@clearscore.com>
2024-03-06 15:36:01 +0000

    adding boardview

--- Snake/Snake/BoardView.swift
+++ Snake/Snake/BoardView.swift
@@ -11,13 +11,13 @@ class BoardView: UIView {
     
     // Here I am creating the dimensions for my grid
     
-    let cols: Int = 9
-    let rows: Int = 7
+    let cols: Int = 19
+    let rows: Int = 17
     
-    let originX: CGFloat = 53
+    let originX: CGFloat = 53 
     let originY: CGFloat = 71
     let cellSide: CGFloat = 23
-
+    
     override func draw(_ rect: CGRect) {
         drawGrid()
     }
@@ -26,10 +26,18 @@ class BoardView: UIView {
         let gridPath = UIBezierPath()
         
         for i in 0...rows{
-            gridPath.move(to: CGPoint(x: originX,y: originX))
-            gridPath.addLine(to: <#T##CGPoint#>)
+            gridPath.move(to: CGPoint(x: originX,y: originY + CGFloat(i) * cellSide))
+            gridPath.addLine(to: CGPoint(x: originX + CGFloat(cols) * cellSide, y: originY + CGFloat(i) * cellSide))
             
         }
+        
+        for i in 0...cols {
+            gridPath.move(to: CGPoint(x: originX + CGFloat(i) * cellSide, y: originY))
+            gridPath.addLine(to: CGPoint(x: originX + CGFloat(i) * cellSide, y: originY + CGFloat(rows) * cellSide))
+            
+        }
+        UIColor.lightGray.setStroke()
+        gridPath.stroke()
         
     }
 
